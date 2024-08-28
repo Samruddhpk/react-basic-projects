958708c5d655d5e84944e5403dbe7550bf2bc881
Samruddh pk <107286956+Samruddhpk@users.noreply.github.com>
2024-08-28 12:35:45 +0530

    submenu-contextAPI

---
+++ 13_Stripe_Submenus/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Hero from "./Hero";
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+import SubMenu from "./SubMenu";
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Sidebar />
+      <SubMenu />
+    </main>
+  );
+};
+export default App;
